df374be35260e0def5ac66e8aa1e88019dedb259
Pavel Filonov <filonovpv@gmail.com>
2015-02-04 20:47:22 +0300

    tempconv with for cycle

--- Lecture1/tempconv.c
+++ Lecture1/tempconv.c
@@ -9,18 +9,10 @@
 
 int main()
 {
-    float farh, celsius;
-    int lower, upper, step;
-
-    lower = 0; /* lower temperature bound for table */
-    upper = 300; /* upper bound */
-    step = 20; /* row step value */
-
-    farh = lower;
-    while (farh <= upper) {
-        celsius = (5.0/9.0) * (farh - 32.0);
-        printf("%3.0f %6.1f\n", farh, celsius);
-        farh = farh + step;
+    float farh;
+    for (farh = 0; farh <= 300; farh = farh + 20) {
+        printf("%3.0f %6.1f\n", farh, (5.0/9.0)*(farh - 32.0));
     }
+
     return 0;
 }
