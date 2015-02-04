9b350994d50c5711eb1cc4463c44e9d5ee3ec066
Pavel Filonov <filonovpv@gmail.com>
2015-02-04 20:44:38 +0300

    tempconv with floats

--- Lecture1/tempconv.c
+++ Lecture1/tempconv.c
@@ -9,7 +9,7 @@
 
 int main()
 {
-    int farh, celsius;
+    float farh, celsius;
     int lower, upper, step;
 
     lower = 0; /* lower temperature bound for table */
@@ -18,8 +18,8 @@ int main()
 
     farh = lower;
     while (farh <= upper) {
-        celsius = 5 * (farh - 32) / 9;
-        printf("%d\t%d\n", farh, celsius);
+        celsius = (5.0/9.0) * (farh - 32.0);
+        printf("%3.0f %6.1f\n", farh, celsius);
         farh = farh + step;
     }
     return 0;
