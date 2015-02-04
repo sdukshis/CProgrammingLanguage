ec3b1ade6927f90942232bb1ca146a1623f0c676
Pavel Filonov <filonovpv@gmail.com>
2015-02-04 20:32:43 +0300

    Hello, world

---
+++ Lecture1/hello.c
@@ -0,0 +1,20 @@
+/* 
+*  This program prints "Hello, world!"
+*  -----------------------------------
+*  Filonov Pavel (filonovpv at gmail.com)
+*  4 Feb 2015
+*/
+
+#include <stdio.h>  /* for printf function */
+
+/* Any C programm start execution from main function */
+int main()
+/* <return value type> <function name>(<formal parameters>) {
+    <body>
+} */
+{
+    printf("Hello, world!\n");  /* \n is a special sybmdol 
+                                   for a newline */ 
+
+    return 0; /* main should returns zero on success */
+}
