10a5747f77568d340d74159563398d9d67819d3b
Pavel Filonov <filonovpv@gmail.com>
2015-02-04 20:49:05 +0300

    tempconv with macro constants

--- Lecture1/tempconv.c
+++ Lecture1/tempconv.c
@@ -7,10 +7,14 @@
 
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main()
 {
     float farh;
-    for (farh = 0; farh <= 300; farh = farh + 20) {
+    for (farh = LOWER; farh <= UPPER; farh = farh + STEP) {
         printf("%3.0f %6.1f\n", farh, (5.0/9.0)*(farh - 32.0));
     }
 
